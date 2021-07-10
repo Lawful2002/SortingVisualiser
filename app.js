bd829c5a330aa0695b6910adebf21c171f7970f3
Harshvardhan <singhharsh5000@gmail.com>
2021-07-11 00:34:32 +0530

    Added BuubbleSort & visual function

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-function bubbleSort(arr){
+function delay(n){
+    return new Promise((res, rej) => {
+        setTimeout(()=>{
+            res();
+        }, n);
+    })
+}
+
+async function bubbleSort(arr){
 
     let n = arr.length;
     
     for(let i=0; i<n; i++){
         for(let j=0; j<n-1-i; j++){
-            if(arr[j]>arr[j+1]){
-                let temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
-            }
-        }
-    }
 
-    printArr(arr);
+            const h1 = parseInt(arr[j].getAttribute("heightnum"))
+            const h2 = parseInt(arr[j+1].getAttribute("heightnum"))              
+            
+
+            arr[j].classList.add("selected")
+            arr[j+1].classList.add("selected")
+
+            await delay(10);
+
+            console.log(`${h1} ${h2}`);
+
+            if(h1 < h2){
+                let tempH = arr[j].style.height;
+                arr[j].style.height = arr[j+1].style.height;
+                arr[j+1].style.height = tempH; 
+
+                let tempA = parseInt(arr[j].getAttribute("heightnum"));
+                arr[j].setAttribute("heightnum", `${parseInt(arr[j+1].getAttribute("heightnum"))}`);
+                arr[j+1].setAttribute("heightnum", `${tempA}`);               
+                
+                
+                let tempC = arr[j].style.backgroundColor;
+                arr[j].style.backgroundColor = arr[j+1].style.backgroundColor;
+                arr[j+1].style.backgroundColor = tempC;
+
+                console.log("swap");
+                
+            }            
+
+            await delay(0);
+            
+
+            arr[j].classList.remove("selected")
+            arr[j+1].classList.remove("selected")
+
+        }
+    }   
 
 }
 
@@ -22,9 +59,9 @@ function randomNum (range, offset = 0){
 
 function randColor (){
     
-    let r = randomNum(255);
-    let g = randomNum(255);
-    let b = randomNum(255);
+    let r = randomNum(255, 35);
+    let g = randomNum(255, 35);
+    let b = randomNum(255, 35);
 
     return `rgb(${r}, ${g}, ${b})`;
 
@@ -36,8 +73,12 @@ function createArr(n){
 
     for(let i=0; i<n; i++){
         const temp = document.createElement("span");
-        temp.style.width = "30px";
-        temp.style.height = `${randomNum(800, 50)}px`;
+        temp.style.width = "30px";    
+        
+        let h = randomNum(500, 50);
+
+        temp.style.height = `${h}px`;
+        temp.setAttribute("heightnum", `${h}`);
         temp.style.backgroundColor = `${randColor()}`;
 
         main.appendChild(temp);
@@ -45,13 +86,16 @@ function createArr(n){
 }
 
 
-let arr = [10, 45, 89, 10, 1, -45, -2, 14, 0, 565, 32];
+// let arr = [10, 45, 89, 10, 1, -45, -2, 14, 0, 565, 32];
 
-function printArr(arr){
-    let n = arr.length;
+// function printArr(arr){
+//     let n = arr.length;
 
-    for(let i=0; i<n; i++){
-        console.log(arr[i]);
-    }
+//     for(let i=0; i<n; i++){
+//         console.log(arr[i]);
+//     }
 
-}
+// }
+
+createArr(150);
+const arr = document.querySelectorAll("span");
